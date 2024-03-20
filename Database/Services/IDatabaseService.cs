f686bfee0da722b5b24e45e6bd437a295b3c11f9
TienDao25 <dtd.dangtiendao@gmail.com>
2024-03-20 22:45:53 +0700

    update getpaging + querymulti

--- Database/Services/IDatabaseService.cs
+++ Database/Services/IDatabaseService.cs
@@ -17,8 +17,24 @@ namespace Database.Services
         public Task<object> GetAllAsync(Type modelType);
         public Task<PagingResponse> GetPagingAsync(Type modelType, PagingRequest pagingRequest);
         #endregion
+
+        #region Query sử dụng command text
         public Task<List<object>> QueryUsingCommandText(string commandText, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+        public Task<List<object>> QueryUsingCommandText(Type modelType, string commandText, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+
+        public Task<List<List<object>>> QueryMultiUsingCommandText(string commandText, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+        public Task<List<List<object>>> QueryMultiUsingCommandText(List<Type> types, string commandText, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+        #endregion
+
+        #region Query sử dụng stored proceducre
+
         public Task<List<object>> QueryUsingStoredProcedure(string storedProcedure, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+        public Task<List<object>> QueryUsingStoredProcedure(Type modelType, string storedProcedure, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+
+        public Task<List<List<object>>> QueryMultiUsingStoredProcedure(string storedProcedure, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+
+        public Task<List<List<object>>> QueryMultiUsingStoredProcedure(List<Type> types, string storedProcedure, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
+        #endregion
 
         public Task<bool> ExecuteScalarCommandText(string commandText, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
         public Task<bool> ExecuteScalarStoredProcedure(string storedProcedure, Dictionary<string, object> dicParam, IDbTransaction transaction = null, IDbConnection connection = null);
